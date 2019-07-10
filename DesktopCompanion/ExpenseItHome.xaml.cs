24b94df5ce1e7f6ddb5643a10554f75e4a244489
Nathan Thompson <nrthompson@mix.wvu.edu>
2019-07-10 16:19:52 -0400

    added files

---
+++ DesktopCompanion/ExpenseItHome.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopCompanion
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            string sSQL = "SELECT * FROM dbo.Person";
+            InitializeComponent();
+            SqlConnection con;
+            SqlCommand sqlCommand;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            //con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|DesktopCompanion.mdf;Integrated Security=True";
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DesktopCompanion.mdf;Integrated Security=True");
+            try
+            {
+                con.Open();
+                sqlCommand = new SqlCommand(sSQL, con);
+                adapter.SelectCommand = sqlCommand;
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+            }
+            peopleListBox.ItemsSource = ds.Tables["Person"].ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ DesktopCompanion/clsDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCompanion
+{
+    public static class clsDB
+    {
+        public static SqlConnection Get_DB_Connecton()
+        {
+            string cn_String = Properties.Settings.Default.database;
+            SqlConnection cn_connection = new SqlConnection(cn_String);
+            if (cn_connection.State != System.Data.ConnectionState.Open) cn_connection.Open();
+
+            return cn_connection;
+        }
+
+        public static DataSet Get_DataTable(string SQL_Text)
+        {
+            SqlConnection cn_connection = Get_DB_Connecton();
+
+            DataSet table = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(SQL_Text, cn_connection);
+            adapter.Fill(table);
+            return table;
+        }
+
+        public static void Execute_SQL(string SQL_Text)
+        {
+            SqlConnection cn_connection = Get_DB_Connecton();
+
+            SqlCommand cmd_Command = new SqlCommand(SQL_Text, cn_connection);
+            cmd_Command.ExecuteNonQuery();
+        }
+    }
+}
